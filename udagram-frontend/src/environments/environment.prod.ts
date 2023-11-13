b20bb3194d5e0b6e110224444d6cea076bf2b470
RokoBasilisk <dhnghia92tcv@gmail.com>
2023-11-14 01:04:54 +0700

    Change apiHost to reverseproxy

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a05a657576f76408b9e140a5ce2d417d-1283207926.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
